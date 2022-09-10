771bc6664c1dfe7c370e600f9c9723fc44a02598
Stenio Joseph <stenioj@tejassoftware.com>
2022-09-10 09:57:14 -0400

    Created music-api project - added models class and controller

---
+++ MusicApi/MusicApi/Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicApi.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+    }
+}
