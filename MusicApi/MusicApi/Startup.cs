9cf84b89b4e40d4308ffa040478393988dfcea93
Stenio Joseph <stenioj@tejassoftware.com>
2022-09-10 14:42:58 -0400

    Injected ApiDbContext class to the startup class

--- MusicApi/MusicApi/Startup.cs
+++ MusicApi/MusicApi/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MusicApi.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,7 @@ namespace MusicApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MusicApi", Version = "v1" });
             });
+            services.AddDbContext<ApiDbContext>(option => option.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MusicDb"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
