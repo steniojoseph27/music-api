af48db338aeaa5dd50196eec4d65bbc789314cff
Stenio Joseph <stenioj@tejassoftware.com>
2022-09-10 14:11:10 -0400

    Added db context class

---
+++ MusicApi/MusicApi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+
+namespace MusicApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
