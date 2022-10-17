e380e796e17fa9efe91b705829c99314306bc52e
Stenio Joseph <stenioj@tejassoftware.com>
2022-10-16 22:17:58 -0400

    Added Image Custome error message Model to the Song

--- MusicApi/MusicApi/Models/Song.cs
+++ MusicApi/MusicApi/Models/Song.cs
@@ -1,10 +1,20 @@
-﻿namespace MusicApi.Models
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicApi.Models
 {
     public class Song
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Title cannot be null or empty")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Language cannot be null or empty")]
         public string Language { get; set; }
+        [Required(ErrorMessage = "Kindly provide the song duration")]
         public string Duration { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
